9bcc21bb9ac35df5d39267b0098e5d302387794d
Sergey Emelainov <sergey.emelyanov@osinit.com>
2017-05-12 16:58:06 +0300

    Add metaclass

--- src/backend/mappers/base.py
+++ src/backend/mappers/base.py
@@ -2,18 +2,34 @@
     This package use for create map from tarantool db to Python object,
     and reverse.
 >>> class TestMapper(BaseMapper):
-...     pass
+...     _map = ('key1', 'key2') 
 ...
 >>> test_object = TestMapper(1,2,3)
->>> test_object._store
-(1, 2, 3)
+>>> test_object._map_dict
+True
 """
 
 
-class BaseMapper(object):
+class MetaMapper(type):
     """
-        Some docstring
+     This class ne
     """
+
+    def __new__(mcs, name, bases, attributes):
+        attributes.update(
+            {'_map_dict': dict(map(
+                lambda elem: (elem[1], elem[0]),
+                enumerate(attributes['_map'])
+            ))}
+        )
+        return type(name, bases, attributes)
+
+
+class BaseMapper(object, metaclass=MetaMapper):
+    """
+     This class ne
+    """
+    _map = ()
 
     def __init__(self, *args):
         self._store = args
